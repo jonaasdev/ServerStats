ddeab72f686bed2e6178ab6fcb61767843389dd1
Jonas Schips <jonas.s@vspedi.de>
2024-06-27 21:51:07 +0200

    Added Automatic Color change for memory usage

--- src/main/java/dev/jonaas/serverstats/ServerStats.java
+++ src/main/java/dev/jonaas/serverstats/ServerStats.java
@@ -7,6 +7,7 @@ import org.bukkit.boss.BarStyle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -18,10 +19,16 @@ public class ServerStats extends JavaPlugin implements CommandExecutor {
     private Map<Player, BossBar> playerBossBars = new HashMap<>();
     private Map<Player, BarColor> playerColors = new HashMap<>();
     private Map<Player, BarStyle> playerStyles = new HashMap<>();
+    private Map<Player, Boolean> playerAutoColorChange = new HashMap<>();
+    // private boolean permissionCheckEnabled = true; // Temporarily deactivated for release build.
 
     @Override
     public void onEnable() {
-        this.getCommand("togglestats").setExecutor(this);
+        saveDefaultConfig();
+        this.getCommand("serverstats").setExecutor(this);
+        // this.getCommand("ss-permcheck").setExecutor(this); // Temporarily deactivated for release build.
+
+        loadPlayerSettings();
 
         new BukkitRunnable() {
             @Override
@@ -41,8 +48,41 @@ public class ServerStats extends JavaPlugin implements CommandExecutor {
             bossBar.removeAll();
         }
         playerBossBars.clear();
+        savePlayerSettings();
     }
 
+    private void loadPlayerSettings() {
+        FileConfiguration config = getConfig();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            String playerUUID = player.getUniqueId().toString();
+            if (config.contains(playerUUID)) {
+                BarColor color = BarColor.valueOf(config.getString(playerUUID + ".color", "WHITE"));
+                BarStyle style = BarStyle.valueOf(config.getString(playerUUID + ".style", "SOLID"));
+                boolean autoColorChange = config.getBoolean(playerUUID + ",autoColorChange", false);
+
+                playerColors.put(player, color);
+                playerStyles.put(player, style);
+                playerAutoColorChange.put(player, autoColorChange);
+
+                BossBar bossBar = Bukkit.createBossBar("Server Stats", color, style);
+                bossBar.addPlayer(player);
+                bossBar.setVisible(true);
+                playerBossBars.put(player, bossBar);
+            }
+        }
+    }
+
+    private void savePlayerSettings() {
+        FileConfiguration config = getConfig();
+        for (Player player : playerBossBars.keySet()) {
+            String playerUUID = player.getUniqueId().toString();
+            config.set(playerUUID + ".color", playerColors.get(player).name());
+            config.set(playerUUID + ".style", playerColors.get(player).name());
+            config.set(playerUUID + ".autoColorChange", playerAutoColorChange.get(player));
+        }
+        saveConfig();
+    }
+ 
     private void updateBossBar(Player player) {
         BossBar bossBar = playerBossBars.get(player);
         int onlinePlayers = Bukkit.getOnlinePlayers().size();
@@ -55,6 +95,10 @@ public class ServerStats extends JavaPlugin implements CommandExecutor {
 
         double progress = (double) freeMemory / maxMemory;
         bossBar.setProgress(Math.min(1.0, Math.max(0.0, progress)));
+
+        if (playerAutoColorChange.getOrDefault(player, false)) {
+            updateBossBarColor(bossBar, freeMemory, maxMemory);
+        }
     }
 
     private double getTPS() {
@@ -70,7 +114,7 @@ public class ServerStats extends JavaPlugin implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("togglestats")) {
+        if (command.getName().equalsIgnoreCase("serverstats")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
 
@@ -79,32 +123,35 @@ public class ServerStats extends JavaPlugin implements CommandExecutor {
                     return true;
                 }
 
-
                 if (args.length == 0) {
-                    toggleStats(player);
+                    serverStats(player);
                 } else if (args.length == 2 && args[0].equalsIgnoreCase("color")) {
                     changeColor(player, args[1]);
                 } else if (args.length == 2 && args[0].equalsIgnoreCase("style")) {
                     changeStyle(player, args[1]);
+                } else if (args.length == 1 && args[0].equalsIgnoreCase("autocolor")) {
+                    toggleAutoColorChange(player);
                 } else {
-                    sender.sendMessage("Usage: /togglestats [string] [value]");
+                    sender.sendMessage("Usage: /serverstats [string] [value]");
                 }
                 return true;
             }
             sender.sendMessage("This command can only be executed by a Player.");
             return true;
+
+        // togglePermission function will be here | Work in Progress
         }
         return false;
     }
 
-    private void toggleStats(Player player ) {
+    private void serverStats(Player player ) {
         if (playerBossBars.containsKey(player)) {
             BossBar bossBar = playerBossBars.get(player);
             bossBar.removeAll();
             playerBossBars.remove(player);
             player.sendMessage("Server statistics are now hidden.");
         } else {
-            BarColor color = playerColors.getOrDefault(player, BarColor.PURPLE);
+            BarColor color = playerColors.getOrDefault(player, BarColor.WHITE);
             BarStyle style = playerStyles.getOrDefault(player, BarStyle.SOLID);
             BossBar bossBar = Bukkit.createBossBar("Server stats", color, style);
             bossBar.addPlayer(player);
@@ -141,4 +188,39 @@ public class ServerStats extends JavaPlugin implements CommandExecutor {
             player.sendMessage("Invalid style.");
         }
     }
+
+    private void updateBossBarColor(BossBar bossBar, long freeMemory, long maxMemory) {
+        double usedMemoryPercentage = (double) (maxMemory - freeMemory) / maxMemory;
+
+        if (usedMemoryPercentage < 0.5) {
+            bossBar.setColor(BarColor.YELLOW);
+        } else if (usedMemoryPercentage < 0.25) {
+            bossBar.setColor(BarColor.RED);
+        } else {
+            bossBar.setColor(BarColor.GREEN);
+        }
+    }
+
+    private void toggleAutoColorChange(Player player) {
+        boolean currentStatus = playerAutoColorChange.getOrDefault(player, false);
+        boolean newStatus = !currentStatus;
+        playerAutoColorChange.put(player, newStatus);
+
+        if (newStatus) {
+            player.sendMessage("Automatic BossBar color change has been enabled.");
+        } else {
+            player.sendMessage("Automatic BossBar color change has been disabled.");
+        }
+    }
+
+    // Function for the toggle operative Permissions | Work in Progress
+    // private void togglePermissionCheck(Player player) {
+    //    permissionCheckEnabled = !permissionCheckEnabled;
+
+    //    if (permissionCheckEnabled) {
+    //        player.sendMessage("ServerStats permission check has been enabled.");
+    //    } else {
+    //        player.sendMessage("ServerStats permission check has been disabled.");
+    //    }
+    //}
 }
